43b437106505ce208f4358900029177e0266c453
yuemenglong <yuemenglong@126.com>
2016-12-07 17:00:55 +0800

    add: 完成对handle到rc的改动，能够返回数据

--- src/rquery.rs
+++ src/rquery.rs
@@ -7,6 +7,9 @@ use std::io::Read;
 use std::iter::Iterator;
 use std::collections::HashMap;
 use std::default::Default;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::ops::Deref;
 use regex::Regex;
 
 use tendril::SliceExt;
@@ -18,6 +21,7 @@ use html5ever::QualName;
 
 use html5ever::rcdom::RcDom;
 use html5ever::rcdom::Handle;
+use html5ever::rcdom::Node;
 use html5ever::rcdom::{Text, Element};
 // use html5ever::rcdom::{Document, Doctype, Text, Comment, Element, Handle};
 
@@ -87,10 +91,10 @@ impl<'a> Cond<'a> {
     }
 }
 
-fn test(handle: Handle, cond_vec: &Vec<Cond>, cond_pos: usize) -> bool {
+fn test(rc: Rc<RefCell<Node>>, cond_vec: &Vec<Cond>, cond_pos: usize) -> bool {
+    let node = rc.borrow();
     let cond = &cond_vec[cond_pos];
-    let handle = handle.borrow();
-    if let Element(ref name, _, ref attrs) = handle.node {
+    if let Element(ref name, _, ref attrs) = node.node {
         let name = name.local.as_ref();
         let mut attrMap: HashMap<&str, &str> = HashMap::new();
         for &Attribute { ref name, ref value } in attrs {
@@ -98,29 +102,28 @@ fn test(handle: Handle, cond_vec: &Vec<Cond>, cond_pos: usize) -> bool {
         }
         let ret = cond.test(&name, &attrMap);
         println!("{:?}", ret);
-        match (ret, cond_pos) {
-            (true, 0) => true,
-            (_, _) => false,
+        match (ret, cond_pos, &node.parent) {
+            // 当前成功且已经完成所有cond，返回成功
+            (true, 0, _) => true,
+            // 当前成功，但还有cond需要测试，却已经到达顶层
+            (true, _, &None) => false,
+            // 当前成功，还有cond需要测试，未到达顶层，继续向上层测试
+            (true, _, &Some(ref parent)) => test(parent.upgrade().unwrap().clone(), cond_vec, cond_pos - 1),
+            (_, _, _) => false,
         }
     } else {
         return false;
     }
 }
 
-fn walk(handle: Handle, cond_vec: &Vec<Cond>) {
-    test(handle.clone(), &cond_vec, 0);
-    for child in handle.borrow().children.iter() {
-        walk(child.clone(), cond_vec);
+fn walk(rc: Rc<RefCell<Node>>, cond_vec: &Vec<Cond>, res_vec: &mut Vec<Rc<RefCell<Node>>>) {
+    if test(rc.clone(), cond_vec, cond_vec.len() - 1) {
+        res_vec.push(rc.clone());
     }
-    // let cur = handle.borrow();
-    // match cur.node {
-    //     Element(ref name, _, ref attrs) => println!("Element {}", name.local),
-    //     Text(ref text) => println!("Text {:?}", text),
-    //     _ => {}
-    // }
-    // for child in cur.children.iter() {
-    //     walk(child.clone(), cond_vec);
-    // }
+    let node = rc.borrow();
+    for child in node.children.iter() {
+        walk(child.deref().clone(), cond_vec, res_vec);
+    }
 }
 
 pub fn selector(str: &str, dom: &RcDom) {
@@ -128,16 +131,12 @@ pub fn selector(str: &str, dom: &RcDom) {
     // let selector = Cond::new();
     let cond_vec = re.find_iter(str)
         .map(|(start, end)| Cond::new(&str[start..end]))
-        .collect::<Vec<Cond>>();
+        // .collect::<Vec<Cond>>();
+        .collect();
+    println!("{:?}", str);
     println!("{:?}", cond_vec);
-    walk(dom.document.clone(), &cond_vec);
-
-    // let vec:Vec<Cond> = Vec::new();
-    // for (start, end) in re.find_iter(str) {
-    // let slice = &str[start..end];
-    // let cond = Cond::new(slice);
-    // cond.parse(slice);
-    // println!("{:?}", cond);
-    // }
-    // println!("{:?}", res);
+    let mut res_vec = Vec::new();
+    let rc = dom.document.deref().clone();
+    walk(rc, &cond_vec, &mut res_vec);
+    println!("{:?}", res_vec);
 }
